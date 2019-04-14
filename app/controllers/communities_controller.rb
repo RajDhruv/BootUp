1e8950edff8a4e35b7feea35f93717e62631c8a7
Faraz Noor <faraznoor75@gmail.com>
2019-04-14 15:08:16 +0530

    Added Loaders and Some tamplate for Validations

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,10 +1,7 @@
 class CommunitiesController < ApplicationController
   #layout "lbd4_application"
+  before_action :set_communities_listing, only: [:index,:create]
   def index
-  	@all_public_communities=Club.where(membership_type:"public_club")
-  	@all_private_communities=Club.where(membership_type:"private_club")
-  	@my_clubs=current_user.clubs
-  	@owned_clubs=Club.where(owner:current_user.id)
   	render partial:"community_router.js.erb",locals:{from: :index}
   end
 
@@ -17,12 +14,20 @@ class CommunitiesController < ApplicationController
   	club=Club.new(club_params)
   	club.owner=current_user.id
   	club.save
-  	redirect_to communities_path
+  	render partial:"community_router.js.erb",locals:{from: :index,notice:"Club Created"}
+  	#TODO noty is not working
   end
 
   private
 
   def club_params
   	params.require(:club).permit(:name, :description,:membership_type,:active)
+  end
+
+  def set_communities_listing
+  	@all_public_communities=Club.where(membership_type:"public_club")
+  	@all_private_communities=Club.where(membership_type:"private_club")
+  	@my_clubs=current_user.clubs
+  	@owned_clubs=Club.where(owner:current_user.id)
   end
 end
