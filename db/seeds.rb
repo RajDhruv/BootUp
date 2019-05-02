52d6422f8bd46e6ab853af2164f16d5ef5c304f6
Faraz Noor <faraznoor75@gmail.com>
2019-05-02 20:25:18 +0530

    Updated Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
+
+50.times do |index|
+	User.create(email:"user_#{index}@example.com",username:"user_#{index}",password:"123456",password_confirmation:"123456")
+end
+puts "50 users created"
+
 dhruv=User.create(email:"raj.drv@gmail.com",username:"dhruv",password:"pleomaxX1!",password_confirmation:"pleomaxX1!")
 puts "Dhruv Created"
+
 faraz=User.create(email:"faraznoor75@gmail.com",username:"faraz",password:"pleomaxX1!",password_confirmation:"pleomaxX1!")
 puts "Faraz Created"
 
-Club.create(name:"Faraz Private",description:"Some sassy Descripttion",active:true,membership_type:1,owner_is:faraz)
+
+faraz_pvt = Club.create(name:"Faraz Private",description:"Some sassy Descripttion",active:true,membership_type:1,owner_is:faraz)
 puts "Faraz Private Created"
 
-
-Club.create(name:"Dhruv Private",description:"Some sassy Descripttion",active:true,membership_type:1,owner_is:dhruv)
+dhruv_pvt = Club.create(name:"Dhruv Private",description:"Some sassy Descripttion",active:true,membership_type:1,owner_is:dhruv)
 puts "Dhruv Private Created"
+
+(1..25).each do |id|
+	faraz_pvt.users<<User.find(id)
+end
+
+(26..50).each do |id|
+	dhruv_pvt.users<<User.find(id)
+end
+
+
+
