aef1ca8f126364bebc781583f5eaed80c5d314a4
Faraz Noor <faraznoor75@gmail.com>
2019-05-12 02:00:58 +0530

    User is now Trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
   has_one :profile , dependent: :destroy
   has_one :preference , dependent: :destroy
   has_one :timeline,as: :timeable,dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -136,6 +136,11 @@ ActiveRecord::Schema.define(version: 2019_05_11_190220) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "roles"
