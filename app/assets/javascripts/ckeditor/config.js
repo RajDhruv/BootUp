221ef2adad57923df2a1a8e75158e23e63d5bb84
Faraz Noor <faraznoor75@gmail.com>
2019-05-23 11:52:29 +0530

    Polish CKEditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -10,5 +10,19 @@ CKEDITOR.editorConfig = function( config ) {
 	config.language = 'en';
   	config.uiColor = '#ffffff';
   	config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
-  	config.removePlugins = "oembed";
+  	config.removePlugins = "oembed,smiley,iframe";
+  	config.extraPlugins = 'emojione';
+  	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] }
+	];
 };
