b2211a5f2a8ef25efb7b77c2918ade51cf4c65a9
Faraz Noor <faraznoor75@gmail.com>
2019-05-29 23:26:01 +0530

    Setting prerequisites for next sprint

--- app/models/enabler.rb
+++ app/models/enabler.rb
@@ -2,8 +2,12 @@ class Enabler < ApplicationRecord
 	belongs_to :enable,polymorphic:true
 	belongs_to :timeline
 	belongs_to :author,class_name:'User'
-
+	scope :friends_posts,->(user) {where(timeline:Timeline.where(user:user.friends))}
+	scope :clubs_posts,->(user){where(timeline:Timeline.where(club:user.clubs))}
+	scope :all_enablers, ->(user) { friends_posts(u).or(clubs_posts(u)) }
 	def name
 		self.enable.name rescue "No Name"
 	end
+
+
 end
