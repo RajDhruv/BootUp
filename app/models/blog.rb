a0df0d0e9e5dfe33cd6a296d14553a300876fa6a
Faraz Noor <faraznoor75@gmail.com>
2019-05-31 12:40:23 +0530

    added serialize methods

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ApplicationRecord
 	has_one :enabler, as: :enable, dependent: :destroy
 	attr_accessor :timeline, :author
 	after_create :create_enabler
-
+	serialize :description
 	def create_enabler
 		Enabler.create(enable: self, timeline: timeline, author: author)
 	end
