ef51bc115e3d07ff2adffbf41aa3ea7493869b49
Faraz Noor <faraznoor75@gmail.com>
2019-06-02 10:20:57 +0530

    removed unwanted code

--- app/models/club.rb
+++ app/models/club.rb
@@ -9,8 +9,7 @@ class Club < ApplicationRecord
 	enum membership_type:{ public_club:0, private_club:1, invite_only:2 }
 	belongs_to :owner_is,class_name:"User",foreign_key: :owner
 	after_create :create_timeline
-  	#after_save :create_timeline
-	scope :public_clubs, -> (user) {where('membership_type=0 and owner != ?',user.id)}
+  	scope :public_clubs, -> (user) {where('membership_type=0 and owner != ?',user.id)}
 	scope :private_clubs, -> (user) {where('membership_type=1 and owner != ?',user.id)}
 	scope :owned_clubs, -> (user) {where(owner_is:user)}
 	def create_timeline
