51be437b3116347aec0d0a0e5224b570a361277b
Dhruv Raj <rajdhruv30@gmail.com>
2019-06-05 12:38:53 +0530

    remove current user from noties

--- app/jobs/enabler_create_job.rb
+++ app/jobs/enabler_create_job.rb
@@ -2,7 +2,8 @@ class EnablerCreateJob < ApplicationJob
   queue_as :urgent
 
   def perform(enabler)
-  	enabler.timeline.subscribers.each do |u|
+  	recipients=enabler.timeline.subscribers-current_user
+  	recipients.each do |u|
 		Notification.create(actor:enabler.author,recipient_id:u,notifiable:enabler.timeline.timeable,action:"Posted a #{enabler.enable.class.to_s}")
 	end
   end
