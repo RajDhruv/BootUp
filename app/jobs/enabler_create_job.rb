8b842d93aba1586907df2253a2db144d03d482ec
Faraz Noor <faraznoor75@gmail.com>
2019-06-05 12:46:15 +0530

    job currnet_user bug

--- app/jobs/enabler_create_job.rb
+++ app/jobs/enabler_create_job.rb
@@ -2,7 +2,7 @@ class EnablerCreateJob < ApplicationJob
   queue_as :urgent
 
   def perform(enabler)
-  	recipients=enabler.timeline.subscribers-current_user
+  	recipients=enabler.timeline.subscribers
   	recipients.each do |u|
 		Notification.create(actor:enabler.author,recipient_id:u,notifiable:enabler.timeline.timeable,action:"Posted a #{enabler.enable.class.to_s}")
 	end
